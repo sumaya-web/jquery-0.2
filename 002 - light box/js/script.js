f25fd3baeac4ac4f08d744961a17c9ff6c44324a
sumayaakter320 <sumaya55230@gmail.com>
2024-02-04 13:12:44 +0600

    [WDN-25] Add light box type 'VenoBox'
    Added VenoBox box to enlarge images and videos on click.

---
+++ 002 - light box/js/script.js
@@ -0,0 +1,17 @@
+new VenoBox({
+    selector:
+        ".my-image-links",
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: "grid",
+});
+
+new VenoBox({
+    selector: ".my-video",
+    spinner: "wave",
+    border: "10px",
+    bgcolor: "#111",
+    infinigall: true,
+    numeration: true,
+});
