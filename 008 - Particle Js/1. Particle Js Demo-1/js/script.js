512b8237a9e377999238653e875caa4374aef006
sumayaakter320 <sumaya55230@gmail.com>
2024-02-04 15:21:31 +0600

    [WDN-31] Add Particle Js Demo-1

---
+++ 008 - Particle Js/1. Particle Js Demo-1/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    Particles.init({
+        selector:
+            ".background",
+        maxParticles: 200,
+        sizeVariations: 5,
+        speed: 1,
+        color: "#fff",
+        minDistance: 300,
+    });
+};
