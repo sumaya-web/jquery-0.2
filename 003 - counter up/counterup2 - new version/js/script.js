711cdf908e7e53449882cf04a0493dc63b4fd961
sumayaakter320 <sumaya55230@gmail.com>
2024-02-04 14:03:11 +0600

    [WDN-26] Add CounterUp2
    Added the new version of counter up js.

---
+++ 003 - counter up/counterup2 - new version/js/script.js
@@ -0,0 +1,36 @@
+const counterUp =
+  window.counterUp.default;
+
+const callback = (
+  entries
+) => {
+  entries.forEach((entry) => {
+    const el = entry.target;
+    if (
+      entry.isIntersecting &&
+      !el.classList.contains(
+        "is-visible"
+      )
+    ) {
+      counterUp(el, {
+        duration: 2000,
+        delay: 16,
+      });
+      el.classList.add(
+        "is-visible"
+      );
+    }
+  });
+};
+
+const IO =
+  new IntersectionObserver(
+    callback,
+    { threshold: 1 }
+  );
+
+const el =
+  document.querySelector(
+    ".counter"
+  );
+IO.observe(el);
